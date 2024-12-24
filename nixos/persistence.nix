6a824ab939bc6d0a395a41fbbc107330d1b054c8
Liberontissauri <jp.liberon@gmail.com>
2024-12-24 14:34:39 +0000

    persistence support

---
+++ nixos/persistence.nix
@@ -0,0 +1,40 @@
+{
+  ...
+}:
+{
+  environment.persistence."/persistent" = {
+    enable = true; 
+    hideMounts = true;
+    directories = [
+      "/var/log"
+      "/var/lib/bluetooth"
+      "/var/lib/nixos"
+      "/var/lib/systemd/coredump"
+    ];
+    files = [
+      "/etc/machine-id"
+      #{ file = "/var/keys/secret_file"; parentDirectory = { mode = "u=rwx,g=,o="; }; }
+    ];
+    users.liberontissauri = {
+      directories = [
+        "Downloads"
+        "Music"
+        "Pictures"
+        "Documents"
+        "Videos"
+        "VirtualBox VMs"
+        { directory = ".gnupg"; mode = "0700"; }
+        { directory = ".ssh"; mode = "0700"; }
+        { directory = ".nixops"; mode = "0700"; }
+        { directory = ".local/share/keyrings"; mode = "0700"; }
+        ".local/share/direnv"
+        # Programs
+        ".config/Bitwarden/"
+        ".mozilla/firefox/" # want to make this more narrowed down in the future
+      ];
+      files = [
+        ".screenrc"
+      ];
+    };
+  };
+}
