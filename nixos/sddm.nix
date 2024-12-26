405933334c3ec856ca121626601f394b32dff7ec
Liberontissauri <jp.liberon@gmail.com>
2024-12-26 01:39:39 +0000

    basic sddm

--- nixos/sddm.nix
+++ nixos/sddm.nix
@@ -1,4 +1,22 @@
 { lib, ... }:
+
 {
-  services.displayManager.sddm.enable = true;
+  services.displayManager.defaultSession = "hyprland";
+  services.displayManager.sddm = {
+    enable = true; # Enable SDDM.
+    wayland.enable = true;
+    sugarCandyNix = {
+        enable = true; # This set SDDM's theme to "sddm-sugar-candy-nix".
+        settings = {
+          # Set your configuration options here.
+          # Here is a simple example:
+          ScreenWidth = 2560;
+          ScreenHeight = 1440;
+          FormPosition = "left";
+          HaveFormBackground = true;
+          PartialBlur = true;
+          # ...
+        };
+      };
+    };
 }
