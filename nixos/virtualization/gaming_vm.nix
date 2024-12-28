665f2d326ae636c250d0408e2e8ee42160185fdd
Liberontissauri <jp.liberon@gmail.com>
2024-12-28 00:22:34 +0000

    started adding libvirt hooks

---
+++ nixos/virtualization/gaming_vm.nix
@@ -0,0 +1,43 @@
+{
+  ...
+}:
+{
+  environment.file."/etc/libvirt/hooks/win10-prepare-begin".text = ''
+      #!/bin/bash
+
+      PCI_GPU_VIDEO="0000:09:00.0"
+      PCI_GPU_AUDIO="0000:09:00.1"
+      ID_GPU_VIDEO="1002 73ff"
+      ID_GPU_AUDIO="1002 ab28"
+      PCI_NVME_1="0000:04:00.0"
+      PCI_NVME_2="0000:05:00.0"
+
+      echo "${PCI_GPU_VIDEO}" > /sys/bus/pci/devices/${PCI_GPU_VIDEO}/driver/unbind
+      echo "${PCI_GPU_AUDIO}" > /sys/bus/pci/devices/${PCI_GPU_AUDIO}/driver/unbind
+      echo "${PCI_NVME_1}" > /sys/bus/pci/devices/${PCI_NVME_1}/driver/unbind
+      echo "${PCI_NVME_2}" > /sys/bus/pci/devices/${PCI_NVME_2}/driver/unbind
+
+      echo "${ID_GPU_VIDEO}" > /sys/bus/pci/drivers/vfio-pci/new_id
+      echo "${ID_GPU_AUDIO}" > /sys/bus/pci/drivers/vfio-pci/new_id
+      echo "${PCI_NVME_1}" > /sys/bus/pci/drivers/vfio-pci/new_id
+      echo "${PCI_NVME_2}" > /sys/bus/pci/drivers/vfio-pci/new_id
+  ''
+  environment.file."/etc/libvirt/hooks/win10-release-end".text = ''
+      #!/bin/bash
+
+      PCI_GPU_VIDEO="0000:09:00.0"
+      PCI_GPU_AUDIO="0000:09:00.1"
+      PCI_NVME_1="0000:04:00.0"
+      PCI_NVME_2="0000:05:00.0"
+
+      echo "${PCI_GPU_VIDEO}" > /sys/bus/pci/drivers/vfio-pci/unbind
+      echo "${PCI_GPU_AUDIO}" > /sys/bus/pci/drivers/vfio-pci/unbind
+      echo "${PCI_NVME_1}" > /sys/bus/pci/drivers/vfio-pci/unbind
+      echo "${PCI_NVME_2}" > /sys/bus/pci/drivers/vfio-pci/unbind
+
+      echo "${PCI_GPU_VIDEO}" > /sys/bus/pci/drivers/amdgpu/bind
+      echo "${PCI_GPU_AUDIO}" > /sys/bus/pci/drivers/snd_hda_intel/bind
+      echo "${PCI_NVME_1}" > /sys/bus/pci/drivers/nvme/bind
+      echo "${PCI_NVME_2}" > /sys/bus/pci/drivers/nvme/bind
+  ''
+}
---
+++ nixos/virtualization/virtualization.nix
@@ -0,0 +1,57 @@
+{ pkgs, config, ... }:
+let
+  user = "liberontissauri";
+
+  platform = "intel";
+
+  vfioIds = [ ];
+in {
+  imports = [
+    ./virtualization/gaming_vm.nix
+  ];
+
+
+  boot = {
+    kernelModules = [ "kvm-${platform}" "vfio_virqfd" "vfio_pci" "vfio_iommu_type1" "vfio" ];
+    kernelParams = [ "${platform}_iommu=on" "${platform}_iommu=pt" "kvm.ignore_msrs=1" ];
+    extraModprobeConfig = "options vfio-pci ids=${builtins.concatStringsSep "," vfioIds}";
+  };
+
+  environment.file."/etc/libvirt/hooks/qemu".text = ''
+      #!/bin/bash
+
+      set -e
+
+      SCRIPT="./${1}-${2}-${3}"
+      if [ -f "$SCRIPT" ]; then
+          . "$SCRIPT"
+      fi
+'' # Separe as {vm}-start-begin and {vm}-start-end (...) scripts
+
+  environment.systemPackages = with pkgs; [
+      virt-manager
+      looking-glass-client
+  ];
+
+  virtualisation = {
+     libvirtd = {
+       enable = true;
+       extraConfig = ''
+         user="${user}"
+       '';
+       onBoot = "ignore";
+       onShutdown = "shutdown";
+
+       qemu = {
+         package = pkgs.qemu_kvm;
+         ovmf.enable = true;
+       };
+    };
+  };
+
+  systemd.tmpfiles.rules = [
+    "f /dev/shm/looking-glass 0660 ${user} qemu-libvirtd -"
+  ];
+
+  users.users.${user}.extraGroups = [ "qemu-libvirtd" "libvirtd" "disk" ];
+}
