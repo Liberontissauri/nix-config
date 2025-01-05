8f130cb3e4410ecba62d65b8c0c04e09d24eebca
Liberontissauri <jp.liberon@gmail.com>
2025-01-05 11:13:21 +0000

    changing things up a bit

---
+++ modules/home/zed/default.nix
@@ -0,0 +1,14 @@
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  options.mine.zed.enable = lib.mkEnableOption "Enable zed";
+  config = lib.mkIf config.mine.zed.enable {
+    home.packages = with pkgs; [
+      zed-editor
+    ];
+    home.file.".config/zed/settings.json".source = config.lib.mkOutOfStoreSymlink ./settings.json;
+  };
+}
