acc656a8447c0388d05fd5815f98eb47f5b09f95
Liberontissauri <jp.liberon@gmail.com>
2025-01-05 15:51:38 +0000

    new firefox options

---
+++ modules/home/firefox/default.nix
@@ -0,0 +1,29 @@
+{
+lib,
+config,
+inputs,
+...
+}:
+{
+  options.mine.firefox.enable = lib.mkEnableOption "Enable firefox";
+  config = lib.mkIf config.mine.zed.enable {
+    programs.firefox.enable = true;
+    programs.firefox.profiles.default = {
+      extensions = with inputs.firefox-addons.packages."x86_64-linux"; [
+              ublock-origin
+              bitwarden
+              darkreader
+              sidebery
+              simple-translate
+            ];
+      settings = {
+        "extensions.autoDisableScopes" = 0;
+        "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+        "layers.acceleration.force-enabled" = true;
+        "gfx.webrender.all" = true;
+        "svg.context-properties.content.enabled" = true;
+      };
+    };
+    home.file.".mozilla/firefox/default/chrome/userChrome.css".source = ./userChrome.css;
+  };
+}
